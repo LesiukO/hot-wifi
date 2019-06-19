01c94ac04c3c8316ca1991159b9a3a26136545e2
Lesiuk Oleksandr <Lesuksashagmail.com>
2019-06-20 02:30:54 +0300

    Реалізував секцію калькулятор

--- js/script.js
+++ js/script.js
@@ -1,9 +1,41 @@
 window.onload = function () {
 	let places = document.querySelectorAll('.title-block__place');
+	let range1 = document.querySelector('#range-1');
+	let range2 = document.querySelector('#range-2');
+	let range3 = document.querySelector('#range-3');
+	let rangeText1 = document.querySelector('#range-text-1');
+	let rangeText2 = document.querySelector('#range-text-2');
+	let rangeText3 = document.querySelector('#range-text-3');
+	let resultNumber1 = document.querySelector('#result__number_1');
+	let resultNumber2 = document.querySelector('#result__number_2');
+	let resultNumber3 = document.querySelector('#result__number_3');
+
+	range1.addEventListener('input', function(e) {
+		e.preventDefault;
+		this.style.background = "-webkit-linear-gradient(left, #317bc7 0%, #317bc7 "+range1.value+"%, #ece7e7 "+range1.value+"%, #ece7e7 100%)";
+		rangeText1.innerHTML = range1.value;
+		resultNumber1.innerHTML = range1.value * 3;
+		resultNumber3.innerHTML = range1.value * range2.value * range3.value;
+	});
+
+	range2.addEventListener('input', function(e) {
+		e.preventDefault;
+		this.style.background = "-webkit-linear-gradient(left, #ffae46 0%, #ffae46 "+range2.value+"%, #ece7e7 "+range2.value+"%, #ece7e7 100%)";
+		rangeText2.innerHTML = range2.value;
+		resultNumber2.innerHTML = range2.value * 2;
+		resultNumber3.innerHTML = range1.value * range2.value * range3.value;
+	});
+
+	range3.addEventListener('input', function(e) {
+		e.preventDefault;
+		this.style.background = "-webkit-linear-gradient(left, #33c964 0%, #33c964 "+range3.value+"%, #ece7e7 "+range3.value+"%, #ece7e7 100%)";
+		rangeText3.innerHTML = range3.value;
+		resultNumber3.innerHTML = range1.value * range2.value * range3.value;
+	});
 
 
 
-	function printNumbersTimeout20_100() {
+	function changeText() {
 	  let i = 1;
 	  let timerId = setTimeout(function go() {
 	    let placeId = places[i - 1].getAttribute('id');
@@ -26,7 +58,7 @@ window.onload = function () {
 	  }, 3000);
 	}
 // вызов
-printNumbersTimeout20_100();
+changeText();
 
 
 }
